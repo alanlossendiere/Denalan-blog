145aa69fa7c13eaab227b80f8b1ea09481bfdbf0
Alan Lossendiere <alanmxzone@hotmail.com>
2022-11-02 16:53:41 -0300

    Se saco email y numero de telefono en formulario de contacto

--- wts-form-contacto.js
+++ wts-form-contacto.js
@@ -2,12 +2,15 @@
 
 function habilitar() {
         let nombre = document.getElementById("nameLastName").value;
-        let email = document.getElementById("email").value;
         let personas = document.getElementById("personas").value;
+        let hospedaje = document.getElementById("hospedaje").value;
         let grupo = document.getElementById("grupo").value;
         val = 0;
 
         if (nombre == "") {
+                val++;
+        }
+        if (personas == "") {
                 val++;
         }
         if (hospedaje == "") {
@@ -16,9 +19,7 @@ function habilitar() {
         if (grupo == "") {
                 val++;
         }
-        if (personas == "") {
-                val++;
-        }
+
         if (val == 0) {
                 document.getElementById("submit-wpp").disabled = false;
         } else {
@@ -26,8 +27,8 @@ function habilitar() {
         }
 }
 document.getElementById("nameLastName").addEventListener("keyup", habilitar);
-document.getElementById("hospedaje").addEventListener("change", habilitar);
 document.getElementById("personas").addEventListener("cheange", habilitar);
+document.getElementById("hospedaje").addEventListener("change", habilitar);
 document.getElementById("grupo").addEventListener("change", habilitar);
 
 /* formulario para enviar */
@@ -45,7 +46,7 @@ const enviarMensaje = () => {
 
 
 
-        var url = window.open("https://api.whatsapp.com/send?phone=5492267520604&text=Mi%20Nombre%20es:%20" + nombre + "%0ATe%20pregunto%20por:%20" + hospedaje + "%0ASomos:%20" + personas + "%20personas      %0ADesde%20el:%20" + fechaEntrada + "%0AHasta%20el:%20" + fechaSalida + "%0AGracias%20:)");
+        var url = window.open("https://api.whatsapp.com/send?phone=5492267520604&text=Mi%20Nombre%20es:%20" + nombre + "%0ATe%20pregunto%20por:%20" + hospedaje + "%0ASomos:%20" + personas + "%20personas%0AUn%20grupo%20de:%20" + grupo + "%0ADesde%20el:%20" + fechaEntrada + "%0AHasta%20el:%20" + fechaSalida + "%0AGracias%20:)");
 
 }
 
