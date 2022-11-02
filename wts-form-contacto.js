83d825ce5c40fc78fb14691e81478d26eb67caed
Alan Lossendiere <alanmxzone@hotmail.com>
2022-11-02 16:41:57 -0300

    Edicion en distintos points

--- wts-form-contacto.js
+++ wts-form-contacto.js
@@ -3,8 +3,6 @@
 function habilitar() {
         let nombre = document.getElementById("nameLastName").value;
         let email = document.getElementById("email").value;
-        let telefono = document.getElementById("number").value;
-        let hospedaje = document.getElementById("hospedaje").value;
         let personas = document.getElementById("personas").value;
         let grupo = document.getElementById("grupo").value;
         val = 0;
@@ -12,19 +10,13 @@ function habilitar() {
         if (nombre == "") {
                 val++;
         }
-        if (email == "") {
-                val++;
-        }
-        if (telefono == "") {
-                val++;
-        }
         if (hospedaje == "") {
                 val++;
         }
-        if (personas == "") {
+        if (grupo == "") {
                 val++;
         }
-        if (grupo == "") {
+        if (personas == "") {
                 val++;
         }
         if (val == 0) {
@@ -34,8 +26,6 @@ function habilitar() {
         }
 }
 document.getElementById("nameLastName").addEventListener("keyup", habilitar);
-document.getElementById("email").addEventListener("keyup", habilitar);
-document.getElementById("number").addEventListener("keyup", habilitar);
 document.getElementById("hospedaje").addEventListener("change", habilitar);
 document.getElementById("personas").addEventListener("cheange", habilitar);
 document.getElementById("grupo").addEventListener("change", habilitar);
@@ -46,8 +36,6 @@ const boton = document.getElementById("submit-wpp")
 
 const enviarMensaje = () => {
         let nombre = document.getElementById("nameLastName").value;
-        let email = document.getElementById("email").value;
-        let telefono = document.getElementById("number").value;
         let hospedaje = document.getElementById("hospedaje").value;
         let personas = document.getElementById("personas").value;
         let grupo = document.getElementById("grupo").value;
@@ -57,8 +45,7 @@ const enviarMensaje = () => {
 
 
 
-        var url = window.open("https://api.whatsapp.com/send?phone=5492267520604&text=Mi%20Nombre%20es:%20" + nombre + "%0ATe%20pregunto%20por:%20" + hospedaje + "%0ASomos:%20" + personas + "%0ASomos%20un%20grupo%20de:%20" + grupo +
-                "%0AMi%20numero%20de%20telefono%20es:%20" + telefono + "%0AMi%20email%20es:%20" + email + "%0ADesde%20el:%20" + fechaEntrada + "%0AHasta%20el:%20" + fechaSalida + "%0AGracias%20:)");
+        var url = window.open("https://api.whatsapp.com/send?phone=5492267520604&text=Mi%20Nombre%20es:%20" + nombre + "%0ATe%20pregunto%20por:%20" + hospedaje + "%0ASomos:%20" + personas + "%20personas      %0ADesde%20el:%20" + fechaEntrada + "%0AHasta%20el:%20" + fechaSalida + "%0AGracias%20:)");
 
 }
 
